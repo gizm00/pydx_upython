54643559d2197291d7016fd2d7813126a1254125
gizm00 <jsev@thedatascout.com>
2016-09-29 13:50:03 -0700

    updating lcd setup

--- setup_lcd.py
+++ setup_lcd.py
@@ -1,21 +1,26 @@
 from machine import Pin, HSPI
+import gc
 import time
 import upcd8544
+
+gc.collect()
+
 import framebuf
+import math
 
-def setup_lcd():
+gc.collect()
 
-	spi = HSPI(baudrate=80000000, polarity=0, phase=0)
-	RST = Pin(4)
-	CE = Pin(5)
-	DC = Pin(12)
-	BL = Pin(16)
-	lcd = upcd8544.PCD8544(spi, RST, CE, DC, BL)
+spi = HSPI(baudrate=80000000, polarity=0, phase=0)
+RST = Pin(4)
+CE = Pin(5)
+DC = Pin(12)
+BL = Pin(16)
+lcd = upcd8544.PCD8544(spi, RST, CE, DC, BL)
 
-	width = 84
-	height = 48
-	pages = height // 8
-	buffer = bytearray(pages * width)
-	framebuf1 = framebuf.FrameBuffer1(buffer, width, height)
+width = 84
+height = 48
+pages = height // 8
+buffer = bytearray(pages * width)
+framebuf1 = framebuf.FrameBuffer1(buffer, width, height)
 
-	return {'lcd':lcd, 'buffer':buffer, 'framebuf1':framebuf1}
+gc.collect()
