8062a57a6083c2dfa0c972e15525ed4320d49b0c
Sev Leonard <jennifer.m.leonard@gmail.com>
2016-11-07 08:46:29 -0800

    deleting old file locations;

--- chdir.py
+++
@@ -1,8 +0,0 @@
-# for moving files around on the ESP8266
-
-def chfiledir(file_in, file_out):
-	with open(file_in) as fin:
-		data = fin.read()
-		fout = open(file_out, 'w')
-		fout.write(data)
-		fout.close()
